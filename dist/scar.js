c9bd4e0351930213e1cdcd2d577766830863ce6e
Lars Jung <lrsjng@gmail.com>
2019-05-03 03:15:36 +0200

    Update.

--- dist/scar.js
+++ dist/scar.js
@@ -582,7 +582,7 @@ module.exports = Suite;
 /* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function(global) {var Err = __webpack_require__(7);
+/* WEBPACK VAR INJECTION */(function(global) {var format_err = __webpack_require__(7);
 
 var Test = __webpack_require__(3);
 
@@ -649,7 +649,7 @@ var reporter = function reporter(type, suite, test) {
     suite.tests.filter(function (t) {
       return t.status === Test.FAILED;
     }).forEach(function (t) {
-      var str = new Err(t.err).format('  ');
+      var str = format_err(t.err, '  ');
       log("\n[".concat(t.failed_idx, "] ").concat(t.desc, "\n").concat(str));
     });
     var resume = '\n';
@@ -677,32 +677,6 @@ module.exports = reporter;
 /***/ }),
 /* 7 */
 /***/ (function(module, exports) {
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
-
-function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
-
-function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 var LINE_PATTERNS = [{
   // v8: ' at <method> (<url>:<line>:<col>)'
@@ -781,60 +755,21 @@ var indent = function indent(str, prefix) {
   return prefix + str.replace(/\n/g, '\n' + prefix);
 };
 
-var Err =
-/*#__PURE__*/
-function (_Error) {
-  _inherits(Err, _Error);
+var format_err = function format_err(err) {
+  var prefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
 
-  function Err(arg) {
-    var _this;
+  var _short3 = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
 
-    _classCallCheck(this, Err);
+  var full_stack = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : false;
+  var frames = parse_stack(err.stack, err.drop);
+  var str = "".concat(err.name, ": ").concat(err.message, "\n");
+  str += indent(format_frames(frames.filter(function (frame) {
+    return full_stack || !frame.drop;
+  }), _short3), '->  ');
+  return indent(str, prefix);
+};
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Err).call(this));
-    _this.name = 'Err';
-    _this.message = '[no message]';
-    _this.drop = 0;
-
-    if (arg) {
-      for (var _i2 = 0, _arr = ['name', 'message', 'stack', 'drop']; _i2 < _arr.length; _i2++) {
-        var prop = _arr[_i2];
-
-        if (arg[prop] !== undefined) {
-          _this[prop] = arg[prop];
-        }
-      }
-    }
-
-    _this.frames = parse_stack(_this.stack, _this.drop);
-    return _this;
-  }
-
-  _createClass(Err, [{
-    key: "format",
-    value: function format() {
-      var prefix = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-
-      var _short3 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
-
-      var full_stack = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-      var str = "".concat(this.name, ": ").concat(this.message, "\n");
-      str += indent(format_frames(this.frames.filter(function (frame) {
-        return full_stack || !frame.drop;
-      }), _short3), '->  ');
-      return indent(str, prefix);
-    }
-  }, {
-    key: "toString",
-    value: function toString() {
-      return this.format();
-    }
-  }]);
-
-  return Err;
-}(_wrapNativeSuper(Error));
-
-module.exports = Err;
+module.exports = format_err;
 
 /***/ }),
 /* 8 */
@@ -844,7 +779,7 @@ module.exports = Err;
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var Err = __webpack_require__(7);
+var format_err = __webpack_require__(7);
 
 var HELP = "\n  scar - a test runner for node and the browser\n\n  Usage:\n    node tests.js [opt...] [arg...]\n    tests.html?opt&...&arg&...\n\n  Options:\n    -h: show this help message\n    -s: show test stats\n\n  Arguments:\n    all arguments are used as test filters\n\n";
 var log = console.log.bind(console);
@@ -896,7 +831,7 @@ var cli = function cli(scar, options) {
           global.process.exit(1);
         }
       })["catch"](function (err) {
-        log("\n".concat(new Err(err).format(), "\n"));
+        log("\n".concat(format_err(err), "\n"));
         log("\n".concat(err.stack, "\n"));
 
         if (global.process) {
